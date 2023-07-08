55d6a63de28fa955caf33c9715cc1f7f6ff5309d
tchen285 <91436200+tchen285@users.noreply.github.com>
2023-07-08 00:05:45 -0700

    Showing 3 recipes info and their images now. Also the images are showing in circle.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Recipes from "./Recipes";
+import Card from "./Card";
+import recipes from "../recipes";
 
 function App() {
     return (
         <div>
             <Header />
-            <Recipes />
+            <Card
+                name={recipes[0].name}
+                img={recipes[0].imgURL}
+                calaries={recipes[0].calaries}
+                cuision={recipes[0].cuision}
+                description={recipes[0].description}
+            />
+            <Card
+                name={recipes[1].name}
+                img={recipes[1].imgURL}
+                calaries={recipes[1].calaries}
+                cuision={recipes[1].cuision}
+                description={recipes[1].description}
+            />
+            <Card
+                name={recipes[2].name}
+                img={recipes[2].imgURL}
+                calaries={recipes[2].calaries}
+                cuision={recipes[2].cuision}
+                description={recipes[2].description}
+            />
             <Footer />
         </div>
     );
---
+++ src/recipes.js
@@ -0,0 +1,25 @@
+const recipes = [
+    {
+        name: "Pizza",
+        imgURL: "./recipe_images/pizza.jpg",
+        calaries: "1000",
+        cuision: "Italian",
+        description: "Yum Italian pizza"
+    },
+    {
+        name: "Lasagna",
+        imgURL: "/recipe_images/lasagna.jpg",
+        calaries: "800",
+        cuision: "Italian",
+        description: "Yum Italian lasagna, Garfield approves"
+    },
+    {
+        name: "Egg Fried Rice",
+        imgURL: "/recipe_images/Hummus.jpg",
+        calaries: "1200",
+        cuision: "Chinese",
+        description: "Uncle Roger Approved"
+    }
+];
+
+export default recipes;
